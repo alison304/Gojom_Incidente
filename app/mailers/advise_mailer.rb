d2731a5e09c84899a5705ef553cd4c43e8bab8ef
Alison Paico <alison.jpq@gmail.com>
2022-03-07 20:58:33 -0500

    actualiza correo de envio

--- app/mailers/advise_mailer.rb
+++ app/mailers/advise_mailer.rb
@@ -1,6 +1,6 @@
 class AdviseMailer < ApplicationMailer
 def sending_incident
     @advice = params[:advice]
-    mail to: "alison.jpq@gmail.com", subject: "incidente", from: "alison.jpq@gmail.com"
+    mail to: "casos@gojom.com", subject: "incidente", from: "alison.jpq@gmail.com"
   end
 end
